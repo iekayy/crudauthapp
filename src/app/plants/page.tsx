e31958afaefab36efa8b7e6f8a37ed2be81cb593
Isaac Koomson <isaackoomson55@gmail.com>
2025-09-21 02:03:35 +0000

    read operation

--- src/app/plants/page.tsx
+++ src/app/plants/page.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { SignUp } from '@stackframe/stack';
 import { stackServerApp } from '@/stack';
 import { InventoryTable } from '@/components/InventoryTable';
+import { getPlants } from '@/actions/plant.action';
 
 
 async function PlantPage() {
 
   const user = await stackServerApp.getUser();
   const app = stackServerApp.urls;
+  const plants = await getPlants();
 
   return (
    <>
@@ -15,7 +17,7 @@ async function PlantPage() {
      {user?(
        <div className="mt-7 max-w-7xl mx-auto px-4 grid grid-cols-1 lg:grid-cols-10 gap-6">
        <div className="lg:col-span-full">
-         <InventoryTable />
+         <InventoryTable plants = {plants} />
        </div>
      </div>
      ):(
--- src/components/InventoryTable.tsx
+++ src/components/InventoryTable.tsx
@@ -12,27 +12,37 @@ import {
 import { Search } from 'lucide-react'
 import { Input } from './ui/input'
 import { Combobox } from './ui/combo-box'
+import { getPlants } from '@/actions/plant.action';
   
-  const plants= [
-    {
-      id: "id419",
-      name: "boba",
-      category: "Inside",
-      price: 2,
-      stock: 10,
-    },
-    
-  ]
   
-  export function InventoryTable() {
 
+  type Plants = Awaited<ReturnType<typeof getPlants>>;
+
+
+  interface InventoryTableProps {
+    plants: Plants;
+  }
+  
+  
+  export function InventoryTable({plants}: InventoryTableProps) {
+    const [searchTerm,setSearchTerm]= useState("");
     const [selectedCategory,setSelectedCategory ] = useState(""); 
+
+
+     // Filter plants by name and category (if selected)
+  const filteredPlants = plants?.userPlants?.filter(
+    (plant) =>
+      plant.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
+      (selectedCategory === "" || plant.category === selectedCategory)
+  );
+  console.log(plants);
+
     return (
 
     <div className='w-full'>
             <div className='flex items-center gap-2 py-4'>
                 <div className='relative max-w--sm w-full'>
-                    <Input placeholder='Filter plants...' className='pl-10'/>
+                    <Input placeholder='Filter plants...' className='pl-10' value={searchTerm} onChange={e=>setSearchTerm(e.target.value)}/>
                     <Search className='absolute h-4 w-4 left-3 top-1/2 transform -translate-y-1/2 '/>
                      
                 </div>
@@ -54,12 +64,15 @@ import { Combobox } from './ui/combo-box'
           </TableRow>
         </TableHeader>
         <TableBody>
-          {plants.map((plant) => (
+          {filteredPlants?.map((plant) => (
             <TableRow key={plant.id}>
+              <TableCell >{plant.id}</TableCell>
               <TableCell >{plant.name}</TableCell>
+
               <TableCell>{plant.category}</TableCell>
               <TableCell>{plant.price}</TableCell>
               <TableCell className='font-bold'>{plant.stock}</TableCell>
+              
 
               <TableCell className="text-right">
                 <div className='flex justify-end space-x-4'>
